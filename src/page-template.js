688da81ed17bd0076acda29c4006c35f0df88db1
Jon Bird <attid@Jons-Laptop>
2022-07-24 23:35:57 -0600

    final html

--- src/page-template.js
+++ src/page-template.js
@@ -4,17 +4,20 @@ const generateManager = function (manager) {
       <div class="card h-100 shadow">
           <div class="card-header bg-primary text-white">
               <h3>${manager.name}</h3>
-              <h4><i class="bi bi-cup-hot"></i> Manager</h4>
+              <h4><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5ZM2.64 13.825 1.123 7h11.754l-1.517 6.825A1.5 1.5 0 0 1 9.896 15H4.104a1.5 1.5 0 0 1-1.464-1.175Z"/>
+              <path d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"/>
+            </svg> Manager</h4>
           </div>
           <div class="card-body bg-light">
             <div class="border bg-white">
-              <p><strong>ID:</strong> ${manager.id}</p>
+              <p class="ml-3"><strong>ID:</strong> ${manager.id}</p>
             </div>
             <div class="border bg-white">
-              <p><strong>Email:</strong> <a href="mailto:${manager.email}">${manager.email}</a></p>
+              <p class="ml-3"><strong>Email:</strong> <a href="mailto:${manager.email}">${manager.email}</a></p>
             </div>
             <div class="border bg-white">
-              <p><strong>Office Number:</strong> ${manager.officeNumber}</p>
+              <p class="ml-3"><strong>Office Number:</strong> ${manager.officeNumber}</p>
             </div>
           </div>
 
@@ -29,17 +32,19 @@ const generateEngineer = function (engineer) {
       <div class="card h-100 bg-light shadow">
           <div class="card-header bg-primary text-white">
               <h3>${engineer.name}</h3>
-              <h4><i class="bi bi-eyeglasses"></i> Engineer</h4>
+              <h4><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+              <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+            </svg> Engineer</h4>
           </div>
           <div class="card-body bg-light">
             <div class="border bg-white">
-              <p><strong>ID:</strong> ${engineer.id}</p>
+              <p class="ml-3"><strong>ID:</strong> ${engineer.id}</p>
             </div>
             <div class="border bg-white">
-              <p><strong>Email:</strong> <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+              <p class="ml-3"><strong>Email:</strong> <a href="mailto:${engineer.email}">${engineer.email}</a></p>
             </div>
             <div class="border bg-white">
-              <p><strong>Github:</strong> <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
+              <p class="ml-3"><strong>Github:</strong> <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
             </div>
           </div>
       </div>
@@ -53,17 +58,20 @@ const generateIntern = function (intern) {
       <div class="card h-100 bg-light shadow">
           <div class="card-header bg-primary text-white">
               <h3>${intern.name}</h3>
-              <h4><i class="bi bi-mortarboard"></i> Intern</h4>
+              <h4><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard" viewBox="0 0 16 16">
+              <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z"/>
+              <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z"/>
+            </svg> Intern</h4>
           </div>
           <div class="card-body bg-light">
             <div class="border bg-white">
-              <p><strong>ID:</strong> ${intern.id}</p>
+              <p class="ml-3"><strong>ID:</strong> ${intern.id}</p>
             </div>
             <div class="border bg-white">
-              <p><strong>Email:</strong><a href="mailto:${intern.email}">${intern.email}</a></p>
+              <p class="ml-3"><strong>Email:</strong> <a href="mailto:${intern.email}">${intern.email}</a></p>
             </div>
             <div class="border bg-white">
-              <p><strong>School:</strong> ${intern.school}</p>
+              <p class="ml-3"><strong>School:</strong> ${intern.school}</p>
             </div>
           </div>
   </div>
